150f58624c7d98eebecec8ee70d71e2fc23de7ad
lory8 <lory8@LAPTOP-P8FBR9SK>
2022-05-24 10:21:14 +0200

    Merge origin/master
    Conflicts:
    	app/Http/Controllers/PublicController.php
    	app/Models/Catalog_.php
    	public/assets/css/style.css
    	resources/views/home.blade.php
    	resources/views/layouts/_navpublic.blade.php
    	routes/web.php

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -13,12 +13,29 @@
                         <img src="http://localhost/laraProj/public/images/products/gigachad.jpg" class="imagefrm">
                      </div>
                     <div class="infopro">
-                        <h2 class="titlepro">Titolo: </h2>
-                        <p class="metapro">Indirizzo </p>
+                        <h2 class="titlepro">Indirizzo </h2>
+                        <div class="adress">
+                            Via cesare cremonini 25
+                        </div>
+                        <div class="propapp">
+                            <div class="casel">
+                                IO
+                            </div>
+                            <div class="casel">
+                                SONO
+                            </div>
+                            <div class="casel">
+                                AGENDA
+                            </div>
+                            
+                        </div>
                         <p class="metapro">Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Phasellus pellentesque. Mauris quam enim, molestie in, rhoncus ut, lobortis a, est. </p>
-                        
                     </div>
                     <div class="pricebox">
+                        <div class="price">
+                            3000€
+                            
+                        </div>
                         <div class="functional-buttons">
                             <ul>
                                 <li><a>Dettagli</a></li>
