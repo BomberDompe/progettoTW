ed91eaa61391b480ca2714c9e9259255a3a889b2
JoshuaSgariglia <S1092411@studenti.univpm.it>
2022-05-25 19:29:37 +0200

    Merge origin/master
    Conflicts:
    	public/assets/css/style.css
    	resources/views/details.blade.php
    	resources/views/layouts/public.blade.php

--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -12,11 +12,12 @@
                    Si consiglia di leggere attentamente il contenuto delle presenti Condizioni Generali di Utilizzo del Sito se si desidera accedere al Sito e consultarne il contenuto.
                 </p>
                 <br> 
-                <h3>Condizioni generali di utilizzo</h3>
+                <h4>Condizioni generali di utilizzo</h4>
                 <p>L’accesso al sito comporta il rispetto e l’accettazione di tutte le presenti Condizioni Generali di Utilizzo del sito. Qualora non si concordi con uno qualsiasi dei termini e delle condizioni delle presenti Condizioni Generali di Utilizzo del sito, l’unico rimedio possibile è quello di interrompere immediatamente la consultazione del sito.</p>
                 <br> 
                 <p>L’accesso al sito è, in linea generale, libero e non è richiesta alcuna registrazione da parte dell’utente. Tuttavia, per l’utilizzo di alcuni servizi del sito può essere richiesta la registrazione dell’utente: la registrazione si effettuerà nella forma espressamente segnalata nel relativo servizio.</p>
-                <h3>Contenuto del sito</h3>
+                <br> 
+                <h4>Contenuto del sito</h4>
                 <p>ApartaRent, in qualità di hosting provider, fornisce spazi sul sito ove utenti privati e soggetti terzi rispetto a ApartaRent (di seguito, gli “Inserzionisti”), possono pubblicare annunci di vendita e/o locazione di immobili nonché – sempre attraverso il sito – gli utenti interessati possono entrare in contatto con gli Inserzionisti.</p>
                 <br>
                 <p>La pubblicazione degli annunci degli Inserzionisti da parte di ApartaRent NON comporta l’approvazione o l’avallo da parte di ApartaRent, né implica alcuna forma di garanzia da parte di quest’ ultima relativamente al contenuto degli stessi. Ai sensi dell’articolo 16 del Decreto Legislativo n. 70/2003, ApartaRent non è responsabile – direttamente ovvero indirettamente – della veridicità, correttezza, completezza, nonché della normativa in materia di privacy e/o di alcun altro aspetto dei suddetti annunci.</p>
@@ -24,13 +25,15 @@
                 <p>ApartaRent – ai sensi dell’articolo 17 del Decreto Legislativo n. 70/2003 – non è, inoltre, assoggettata ad un obbligo generale di sorveglianza sul contenuto degli annunci degli Inserzionisti, né ad un obbligo generale di ricercare attivamente fatti o circostanze che indichino la presenza di attività illecite.</p>
                 <br>
                 <p>In generale, le informazioni presenti sul sito non potranno essere considerate in alcun caso come sostitutive di pareri professionali legali, finanziari o in materia immobiliare. ApartaRent, i suoi amministratori, dirigenti e agenti declinano ogni responsabilità per qualsiasi danno o perdita, diretta o indiretta, cagionati a qualunque soggetto – persona fisica o giuridica – che abbia fatto affidamento su qualsiasi contenuto – ivi inclusi i Materiali Inserzionisti e i Materiali ApartaRent – presente sul sito.</p>
-                <h3>Proprietà intellettuale </h3>
+                <br> 
+                <h4>Proprietà intellettuale </h4>
                 <p>Gli Inserzionisti sono titolari di tutti diritti di proprietà relativi a video, fotografie, fermo immagini, documenti o altro materiale pubblicati dagli stessi sui Siti attraverso gli annunci (di seguito, i “Materiali Inserzionisti”).</p>
                 <br> 
                 <p>Fatti, appunto, salvi i Materiale Inserzionisti, ApartaRent è titolare ovvero, a seconda del caso, licenziataria di tutti diritti di proprietà del sito, delle pagine del sito, delle informazioni o elementi contenuti nei testi, documenti, fotografie, disegni, grafica, software, loghi, marchi, nomi commerciali, etc. (di seguito, i “Materiali ApartaRent”).</p>
                 <br> 
                 <p>Pertanto, l’utente del sito non può – in assenza di licenza d’uso da parte di ApartaRent ovvero degli Inserzionisti – in nessun caso riprodurre, modificare, decodificare, distribuire, copiare o divulgare i Materiali Inserzionisti ovvero i Materiali ApartaRent</p>
-                <h3>Limitazioni di responsabilità ApartaRent</h3>
+                <br> 
+                <h4>Limitazioni di responsabilità ApartaRent</h4>
                 <p>Nei limiti consentiti dalle disposizioni di legge applicabili, in nessun caso ApartaRent potrà essere ritenuta responsabile nei confronti dell’utente del sito ovvero anche degli Inserzionisti o di terzi per qualsiasi danno diretto, indiretto, consequenziale, di natura economica e non inclusi, a titolo esemplificativo, il lucro cessante, mancato guadagno, perdita, di clientela, da interruzione di attività, perdita di profitto, danno all’immagine, perdita di chance, perdita di dati, perdita di spese anticipate e di guadagni derivanti o relativi all’accesso/consultazione/utilizzo del sito.</p>
             </div>
         </div>
