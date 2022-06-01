e35ed9809781eb375464d895f66482f63a71675a
Lorenzo <Lorenzo@DESKTOP-RKSUVOI>
2022-06-01 10:21:36 +0200

    Merge origin/master
    Conflicts:
    	app/Http/Controllers/PublicController.php
    	app/Http/Controllers/UserController.php
    	app/Models/Catalog.php
    	app/Models/Resources/Conversation.php
    	app/Models/Resources/Faq.php
    	app/Models/Resources/Offer.php
    	app/Providers/AuthServiceProvider.php
    	database/migrations/2022_05_19_174349_offers_migration.php
    	database/migrations/2022_05_19_174434_chats_migration.php
    	database/seeds/DatabaseSeeder.php
    	public/assets/css/style.css
    	resources/views/catalog.blade.php
    	resources/views/details.blade.php
    	resources/views/faqs.blade.php
    	resources/views/icons/checkmark.php
    	resources/views/layouts/_navpublic.blade.php
    	resources/views/layouts/public.blade.php
    	resources/views/pagination/paginator.blade.php
    	routes/web.php

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -10,82 +11,16 @@
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
 
         <title>ApartaRent - @yield('title', 'Home')</title>
+        
+        <!-- Icona -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
         <!-- CSS Files -->
         <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
-        <!-- Include file JavaScript -->
+         <!-- Include file JavaScript -->
         @stack('custom-scripts')
-
-    </head>
-
-    <body>
-        <!-- Start Header -->
-        <header>
-            <nav class="navbar navbar-expand-lg">
-                <div class="container">
-                    @include('layouts/_navpublic')
-                </div>
-            </nav>
-        </header>
-        <!-- end #header/menù --> 
-
-        <!--Start content-->
-
-        <div class ="content">
-            @yield('content')
-        </div>
-
-        <!-- Footer Starts Here -->
-        <footer>
-            <div class="container" style="height: 40px;">
-                <h2 style="color: white;">ApartaRent </h2>
-                <h4 style="color: whitesmoke;position: relative;left: 350px;font-size: small;font-style: oblique;font-family: cursive;">l'appartamento giusto per voi</h4>
-            </div>
-            <div class="container" style="border-top: outset;">
-                <a href="#" class="fa fa-facebook"></a>
-                <a href="#" class="fa fa-twitter"></a>
-                <a href="#" class="fa fa-linkedin"></a>
-                <a href="#" class="fa fa-youtube"></a>
-                <a href="#" class="fa fa-instagram"></a>
-            </div>
-        </footer>
-        <!-- Footer Ends Here -->
-
-        <!-- PopUp Start -->    
-        <div id="popuplog" class="overlayppu">
-            <div class="popuplog">
-                <a class="close" href="#">&times;</a>
-                @include('auth/login')
-            </div>
-        </div>
-
-        <div id="popupreg" class="overlayppu">
-            <div class="popupreg">
-                <a class="close" href="#">&times;</a>
-                @include('auth/register') 
-            </div>
-        </div>
-        <!-- PopUp End -->
-    </body>
-</html>
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-    <head>
-
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="Progetto TWeb">
-        <meta name="author" content="Gruppo 24">
-        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
-
-        <title>ApartaRent - @yield('title', 'Home')</title>
-
-        <!-- CSS Files -->
-        <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
     </head>
 
